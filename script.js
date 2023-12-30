0db55ebb0f4a002bb2397de2837a2a2ed59a6deb
Santam Kumai <santtamk@gmail.com>
2023-12-30 10:00:29 +0530

    append method simplified

--- script.js
+++ script.js
@@ -3,25 +3,40 @@
 
 class Node {
     constructor(value){
-        this.value = null
+        this.value = value
         this.nextNode = null
     }
 }
 
 
-class LinkedList {
+class SinglyLinkedList {
     constructor(){
-        this.head = null
+        this.head = null;
+        this.tail = null;
         this.size = 0
     }
     
-    // append(value) adds a new node containing value to the end of the list
-    append(value){
-        const newNode = new Node(value)
-        newNode.nextNode = this.head
-        this.head = newNode
-        this.size++;
+    // append/push(value) adds a new node containing value to the end of the list
+    append(value){ //this function should accept a value
+        const newNode = new Node(value)//create a new node using the value passed to the function
+        if(!this.head){
+            this.head = newNode;
+            //if there is no head property on the list, set the head and tail to be the new created node
+            this.tail = this.head;
+        }else{
+            //otherwise set the next property on the tail to be the new node and set the tail property on the list to be the newly created node
+            this.tail.nextNode = newNode;
+            this.tail = newNode;
+        }
+        this.size++;//increment the length by one 
+        return this;
     }
 
-    
+    // prepend(value){
+    //     const newNode = new Node(value)
+    //     // newNode.
+    // }
 }
+
+let list = new SinglyLinkedList();
+// list.append("HELLO")
